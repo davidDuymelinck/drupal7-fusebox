0447d18a7cb6cba8fc7fdea1ca53eac091ec0f73
davidDuymelinck <dlinck.d+github@gmail.com>
2017-07-28 20:18:11 +0200

    used fusebox task runner to create production CLI command

--- fuse.js
+++ fuse.js
@@ -1,14 +1,38 @@
-const { FuseBox, CSSPlugin, SassPlugin, WebIndexPlugin } = require("fuse-box");
+const { FuseBox, CSSPlugin, SassPlugin, UglifyJSPlugin, Sparky } = require("fuse-box");
 
-const fuse = FuseBox.init({
-    homeDir: "theme_assets",
-    output: "$name.js"
+let fuse, myTheme, isProduction = false;
+
+Sparky.task("config", () => {
+    fuse = FuseBox.init({
+        homeDir: "theme_assets",
+        output: "$name.js",
+        sourceMaps: !isProduction,
+        plugins: [
+            isProduction && UglifyJSPlugin()
+        ]
+    });
+
+    myTheme = fuse.bundle("sites/all/themes/my_theme/js/main")
+        .instructions("> my_theme/index.ts");
 });
 
-fuse.bundle("sites/all/themes/my_theme/js/main")
-    .plugin(SassPlugin(), CSSPlugin({
-        outFile: (file) => `sites/all/themes/my_theme/css/main.css`
-    }))
-    .instructions("> my_theme/index.ts");
+const myThemeCSSConfig = { outFile: (file) => `sites/all/themes/my_theme/css/main.css` };
 
-fuse.run();
+Sparky.task("default", ["config"], () => {
+    myTheme.plugin(SassPlugin(), CSSPlugin(myThemeCSSConfig)).watch();
+    return fuse.run();
+});
+
+
+Sparky.task("live", ["set-production", "config"], () => {
+    myTheme.plugin(SassPlugin({ outputStyle: 'compressed' }), CSSPlugin(myThemeCSSConfig));
+    return fuse.run();
+});
+
+Sparky.task("set-production", () => {
+    isProduction = true;
+});
+
+
+
+
