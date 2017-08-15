5adecb733d2d0c5e9b10a8b5de8c10d1b9033e87
davidDuymelinck <dlinck.d+github@gmail.com>
2017-08-15 15:42:44 +0200

    removed all drupal js and css. Made the theme more shoelacey.

--- sites/all/themes/my_theme/template.php
+++ sites/all/themes/my_theme/template.php
@@ -1,12 +1,5 @@
 <?php
 
-function my_theme_js_alter(&$js) {
-  unset(
-    $js['misc/drupal.js'],
-    $js['misc/jquery.js'],
-    $js['misc/ui/jquery.ui.core.min.js'],
-    $js['misc/jquery.once.js'],
-    $js['misc/jquery.ba-bbq.js'],
-    $js['misc/jquery.cookie.js']
-  );
+function my_theme_preprocess(&$variables, $hook) {
+  $variables['theme_path'] = drupal_get_path('theme', 'my_theme');
 }
